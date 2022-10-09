2e3fdaa468fee60b7f83efdba01a1e140229740e
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-10-09 17:23:59 +0330

    ..

--- Cover.py
+++ Cover.py
@@ -12,11 +12,14 @@ from telegram import (
     ReplyKeyboardMarkup, 
 )
 
+import moviepy
+import moviepy.editor 
+
 import localization as lp
 from utils import translate_key_to, reset_user_data_context, generate_start_over_keyboard, \
 create_user_directory, download_file, increment_usage_counter_for_user, delete_file, \
-generate_module_selector_keyboard, generate_module_selector_video_keyboard, generate_tag_editor_keyboard, generate_music_info, \
-save_tags_to_file
+generate_module_selector_keyboard, generate_module_selector_video_keyboard, generate_tag_editor_keyboard, \
+generate_music_info, generate_tag_editor_video_keyboard, save_tags_to_file, convert_video
 
 from models.admin import Admin
 from models.user import User
@@ -378,11 +381,12 @@ def convert_mp4_to_webm(update: Update, context: CallbackContext) -> None:
     tag_editor_context = user_data['tag_editor']
     tag_editor_context['current_tag'] = ''
 
-    tag_editor_keyboard = generate_tag_editor_keyboard(lang)
+    tag_editor_keyboard = generate_tag_editor_video_keyboard(lang)
     chat_id = update.message.chat_id
 
     if video_path:
-        with open(video_path.format(chat_id, 'webm'), 'rb') as video_file:
+        # with open(video_path.format(chat_id, "webm"), 'rb') as video_file:
+        with open(video_path, 'rb') as video_file:
             # video_file = file_name.split(".")[0]
             # logger.error(video_file)
             #   video_file = str(video_file) +'.webm'
@@ -392,12 +396,41 @@ def convert_mp4_to_webm(update: Update, context: CallbackContext) -> None:
             #     document=open('./output_media/{}.{}'.format(chat_id, 'webm'), 'rb'), 
             #     caption="Here is your file!"
             # )
-            message.reply_video(
-                video=video_file,
+            # logger.info(video_file)
+            # convert_video(chat_id, video_path, "webp")
+            # v = moviepy.VideoFileClip(video_path)
+            # v.write_videofile()
+
+            # video = moviepy.editor.VideoFileClip(video_path)
+            # audio = video.audio
+            # video.write_videofile(filename + ".wav")
+
+            # video_file = moviepy.editor.VideoFileClip(video_path)
+            # wav_file_name = video_path.replace('.mp4', '.webm')  # Replace .mkv with .wav
+            # video_file.write_videofile(wav_file_name)
+
+            # context.bot.send_document(
+            #     chat_id=chat_id, 
+            #     document=open(video_path.format(chat_id, "webp"), 'rb'), 
+            #     caption="Here is your file!"
+            # )
+            # context.bot.send_video(
+            #     chat_id=update.message.chat_id, 
+            #     video=open('output.mp4', 'rb'), 
+            #     supports_streaming=True
+            # )
+
+            message.reply_video_note(
+                video_note=video_file,
                 reply_to_message_id=update.effective_message.message_id,
                 reply_markup=tag_editor_keyboard,
-                parse_mode='Markdown'
-            )   
+            )
+            # message.reply_video(
+            #     video=video_file,
+            #     reply_to_message_id=update.effective_message.message_id,
+            #     reply_markup=tag_editor_keyboard,
+            #     parse_mode='Markdown'
+            # )   
     else:
         message.reply_text(
             generate_music_info(tag_editor_context).format(f"\n🆔 {BOT_USERNAME}"),
